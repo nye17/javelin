78aeb15bedef50df546d133dbfd9b6a8f5dcfc53
Ying <nye17@users.noreply.github.com>
2011-10-30 16:58:28 -0400

    add return value to anaMCMC function@mitchell

--- pyspear/bayesian/data.py
+++ pyspear/bayesian/data.py
@@ -2,8 +2,9 @@ import pyspear.lcio as IO
 from pyspear.zylc import zyLC
 import numpy as np
 
+#lcfile = "dat/mock_l100c1_t10s2n0.5.dat"
+
 def get_data(lcfile):
-    #lcfile = "dat/mock_l100c1_t10s2n0.5.dat"
     lclist = IO.readlc_3c(lcfile)
     zydata = zyLC(lclist)
     return(zydata)

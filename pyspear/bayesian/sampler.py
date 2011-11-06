103ff93ea044f0e9112b2ed0f664ece835c8d22d
Ying Zu <zuying@gmail.com>
2011-11-06 01:27:23 -0500

    add peakdetect.py and finish ogle_example_4, update to arjuna so that a test with mock light curves can be done through the night@sing-sing

--- pyspear/bayesian/sampler.py
+++ pyspear/bayesian/sampler.py
@@ -1,4 +1,4 @@
-#Last-modified: 05 Nov 2011 02:06:47 AM
+#Last-modified: 06 Nov 2011 01:26:34 AM
 import numpy as np
 import pickle
 import os.path
@@ -7,6 +7,9 @@ import pymc as pm
 from prh import PRH
 from data import get_data
 from model import make_model_powexp
+from peakdetect import peakdet1d, peakdet2d
+
+import matplotlib.pyplot as plt
 
 from pyspear.traceless import TraceLess 
 
@@ -116,14 +119,21 @@ def getValues(M):
         tovary.append(np.atleast_1d(M.nu.value)[0])
     return(tovary)
 
-def varying_tau(output, zydata, tauarray):
+def varying_tau(output, zydata, tauarray, fixednu=None, set_verbose=False):
     """ grid optimization along tau axis.
     """
     result = []
+    if fixednu is None:
+        use_nuprior="Uniform"
+    elif is_number(fixednu):
+        use_nuprior=fixednu
+    else:
+        raise RuntimeError("no such fixednu option")
     for tau in tauarray:
-        model   = make_model_powexp(zydata, use_sigprior="None", use_tauprior=tau, use_nuprior="Uniform")
-        bestpar = list(runMAP(model))
-        testout = list(getPlike(zydata, bestpar))
+        print("tau: %10.5f"%tau)
+        model   = make_model_powexp(zydata, use_sigprior="None", use_tauprior=tau, use_nuprior=use_nuprior)
+        bestpar = list(runMAP(model, set_verbose=set_verbose))
+        testout = list(getPlike(zydata, bestpar, set_verbose=set_verbose))
         result.append(" ".join(format(r, "10.4f") for r in bestpar+testout)+"\n")
     f=open(output, "w")
     f.write("".join(result))
@@ -162,21 +172,53 @@ def read_grid_tau_nu(input):
     sigma, tau, nu, loglike, chi2, complexity, drift, q = np.genfromtxt(f, unpack=True)
     f.close()
     retdict = {
-               'sigma'          :        sigma.reshape(dim_tau, dim_nu)
-               'tau'            :          tau.reshape(dim_tau, dim_nu)
-               'nu'             :           nu.reshape(dim_tau, dim_nu)
-               'loglike'        :      loglike.reshape(dim_tau, dim_nu)
-               'chi2'           :         chi2.reshape(dim_tau, dim_nu)
-               'complexity'     :   complexity.reshape(dim_tau, dim_nu)
-               'drift'          :        drift.reshape(dim_tau, dim_nu)
-               'q'              :            q.reshape(dim_tau, dim_nu)
+               'sigma'          :        sigma.reshape(dim_tau, dim_nu),
+               'tau'            :          tau.reshape(dim_tau, dim_nu),
+               'nu'             :           nu.reshape(dim_tau, dim_nu),
+               'loglike'        :      loglike.reshape(dim_tau, dim_nu),
+               'chi2'           :         chi2.reshape(dim_tau, dim_nu),
+               'complexity'     :   complexity.reshape(dim_tau, dim_nu),
+               'drift'          :        drift.reshape(dim_tau, dim_nu),
+               'q'              :            q.reshape(dim_tau, dim_nu),
               }
     return(retdict)
 
-def show_loglike_map():
-    """ Process the 2D likelihood maps.
+def show_loglike_map(x, y, z, ax, cax=None, 
+                     set_contour=True, clevels=None,
+                     vmin=None, vmax=None, xlabel=None, ylabel=None, zlabel=None, 
+                     set_normalize=True, peakpos=None, cmap='jet'):
+    """ Display the 2D likelihood maps, assuming xmesh and ymesh are on regular grids.
     """
-    pass
+    xmin,xmax,ymin,ymax = np.min(x),np.max(x),np.min(y),np.max(y)
+    extent = (xmin,xmax,ymin,ymax)
+    if set_normalize:
+        zmax = np.max(z)
+        z    = z - zmax
+    if vmin is None:
+        vmin = z.min()
+    if vmax is None:
+        vmax = z.max()
+    im = ax.imshow(z, origin='lower', vmin=vmin, vmax=vmax,
+                      cmap=cmap, interpolation="nearest", aspect="auto", extent=extent)
+    if set_contour:
+        if clevels is None:
+            sigma3,sigma2,sigma1 = 11.8/2.0,6.17/2.0,2.30/2.0
+            levels = (vmax-sigma1, vmax-sigma2, vmax-sigma3)
+        else:
+            levels = clevels
+        ax.set_autoscale_on(False)
+        cs = ax.contour(z,levels, hold='on',colors='k',
+                          origin='lower',extent=extent)
+    if xlabel:
+        ax.set_xlabel(xlabel)
+    if ylabel:
+        ax.set_ylabel(ylabel)
+    if peakpos:
+        ax.set_autoscale_on(False)
+        ax.plot(x[peakpos],y[peakpos], 'wx', markersize=20)
+    if cax:
+        cb = plt.colorbar(im, cax=cax)
+        cb.ax.set_ylabel(zlabel)
 
 
 def pretty_array(x):
@@ -228,14 +270,69 @@ def ogle_example_2():
     varying_tau_nu(record, zydata, tauarray, nuarray, set_verbose=False)
     print("done!")
 
-if __name__ == "__main__":    
+def ogle_example_3():
+    """ yet another example, testing 2D grid visualization and peak finding.
+    """
     ttau = 200.0
     tnu = 1.4
-    record = "dat/OGLE_example/pow_exp_T"+str(ttau)+"_N"+str(tnu)+".test2dgrid.dat"
-    read_grid_tau_nu(record)
+    record   = "dat/OGLE_example/pow_exp_T"+str(ttau)+"_N"+str(tnu)+".test2dgrid.dat"
+    griddict = read_grid_tau_nu(record)
+    print(griddict.keys())
+    fig = plt.figure(figsize=(6, 10))
+    ax1  = fig.add_axes([0.2, 0.1, 0.55, 0.25])
+    cax1 = fig.add_axes([0.75, 0.1, 0.02, 0.25])
+    ax2  = fig.add_axes([0.2, 0.4, 0.55, 0.25])
+    cax2 = fig.add_axes([0.75, 0.4, 0.02, 0.25])
+    ax3  = fig.add_axes([0.2, 0.7, 0.55, 0.25])
+    cax3 = fig.add_axes([0.75, 0.7, 0.02, 0.25])
+    p1 = griddict['loglike']
+    p2 = griddict['chi2']
+    p3 = griddict['complexity']
+    detected_peaks1 = peakdet2d(p1)
+    peakpos1 = np.where(detected_peaks1)
+    detected_peaks2 = peakdet2d(p2)
+    peakpos2 = np.where(detected_peaks2)
+    detected_peaks3 = peakdet2d(p3)
+    peakpos3 = np.where(detected_peaks3)
+    show_loglike_map(griddict['nu'], np.log10(griddict['tau']), p1, ax1, cax=cax1, 
+                     set_contour=True, clevels=None,
+                     vmin=-10, vmax=0, 
+                     xlabel="$\\nu$", ylabel="$\log\\tau$", zlabel="$\ln \mathcal{L}$",
+                     set_normalize=True, peakpos=peakpos1, cmap='jet')
+    show_loglike_map(griddict['nu'], np.log10(griddict['tau']), p2, ax2, cax=cax2, 
+                     set_contour=True, clevels=None,
+                     vmin=-10, vmax=0, 
+                     xlabel="$\\nu$", ylabel="$\log\\tau$", zlabel="$\chi^2/2$",
+                     set_normalize=True, peakpos=peakpos2, cmap='jet')
+    show_loglike_map(griddict['nu'], np.log10(griddict['tau']), p3, ax3, cax=cax3, 
+                     set_contour=True, clevels=None,
+                     vmin=-10, vmax=0, 
+                     xlabel="$\\nu$", ylabel="$\log\\tau$", zlabel="$\mathrm{complexity}$",
+                     set_normalize=True, peakpos=peakpos3, cmap='jet')
+    plt.show()
+
+def ogle_example_4():
+    """ another OGLE example, testing 1d grid optimization using 'varying_tau' but with nu fixed.
+    """
+    tauarray = np.power(10.0, np.arange(0, 4.2, 0.1))
+    truetau  = np.array([20.0, 40.0, 60.0, 80.0, 100.0, 200.0, 400.0, 600.0, 800.0, 1000.0])
+    truenu   = np.array([0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8])
+    fixednu = 1.0
+    ttau = 200.0
+    tnu = 1.4
+    for ttau in truetau:
+        for tnu in truenu:
+            lcfile = "dat/OGLE_example/pow_exp_T"+str(ttau)+"_N"+str(tnu)+".dat"
+            print("reading %s"%lcfile)
+            zydata  = get_data(lcfile)
+            record = "dat/OGLE_example/pow_exp_T"+str(ttau)+"_N"+str(tnu)+".testtau_DRW.dat"
+            print("writing %s"%record)
+            varying_tau(record, zydata, tauarray, fixednu=fixednu, set_verbose=False)
+    print("done!")
 
 
-
+if __name__ == "__main__":    
+    ogle_example_4()
 
 
 

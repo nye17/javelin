981c38ff0f18d7746d7e023eda52dd73abea7d54
Ying <nye17@users.noreply.github.com>
2012-01-17 12:19:47 -0500

    want to make psd work@mitchell

---
+++ javelin/psd.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+#------------------------------------------------------------------------------
+# Filename: psd.py
+#  Purpose: Various Routines Related to Spectral Estimation
+#   Author: Tobias Megies
+#    Email: tobias.megies@geophysik.uni-muenchen.de
+#
+# Copyright (C) 2011 Tobias Megies
+#------------------------------------------------------------------------------
+"""
+Various Routines Related to Spectral Estimation
+
+:copyright:
+    The ObsPy Development Team (devs@obspy.org)
+:license:
+    GNU Lesser General Public License, Version 3
+    (http://www.gnu.org/copyleft/lesser.html)
+"""
+
+from __future__ import with_statement
+import os
+import warnings
+import pickle
+import math
+import numpy as np
+
+def getMatplotlibVersion():
+    """
+    Get matplotlib version information.
+
+    :returns: Matplotlib version as a list of three integers or ``None`` if
+        matplotlib import fails.
+    """
+    try:
+        import matplotlib
+        version = matplotlib.__version__.replace('svn', '')
+        version = map(int, version.split("."))
+    except ImportError:
+        version = None
+    return version
+
+MATPLOTLIB_VERSION = getMatplotlibVersion()
+
+if MATPLOTLIB_VERSION == None:
+    # if matplotlib is not present be silent about it and only raise the
+    # ImportError if matplotlib actually is used (currently in psd() and
+    # PPSD())
+    msg_matplotlib_ImportError = "Failed to import matplotlib. While this " \
+            "is no dependency of obspy.signal it is however necessary for a " \
+            "few routines. Please install matplotlib in order to be able " \
+            "to use e.g. psd() or PPSD()."
+    # set up two dummy functions. this makes it possible to make the docstring
+    # of psd() look like it should with two functions as default values for
+    # kwargs although matplotlib might not be present and the routines
+    # therefore not usable
+
+    def detrend_none():
+        pass
+
+    def window_hanning():
+        pass
+
+else:
+    # Import matplotlib routines. These are no official dependency of
+    # obspy.signal so an import error should really only be raised if any
+    # routine is used which relies on matplotlib (at the moment: psd, PPSD).
+    from matplotlib import mlab
+    import matplotlib.pyplot as plt
+    from matplotlib.dates import date2num
+    from matplotlib.ticker import FormatStrFormatter
+    from matplotlib.colors import LinearSegmentedColormap
+    from matplotlib.mlab import detrend_none, window_hanning
+
+
+# do not change these variables, otherwise results may differ from PQLX!
+PPSD_LENGTH = 3600  # psds are calculated on 1h long segments
+PPSD_STRIDE = 1800  # psds are calculated overlapping, moving 0.5h ahead
+
+
+def psd(x, NFFT=256, Fs=2, detrend=detrend_none, window=window_hanning,
+        noverlap=0):
+    """
+    Wrapper for :func:`matplotlib.mlab.psd`.
+
+    Always returns a onesided psd (positive frequencies only), corrects for
+    this fact by scaling with a factor of 2. Also, always normalizes to dB/Hz
+    by dividing with sampling rate.
+
+    This wrapper is intended to intercept changes in
+    :func:`matplotlib.mlab.psd` default behavior which changes with
+    matplotlib version 0.98.4:
+
+    * http://matplotlib.sourceforge.net/users/whats_new.html\
+#psd-amplitude-scaling
+    * http://matplotlib.sourceforge.net/_static/CHANGELOG
+      (entries on 2009-05-18 and 2008-11-11)
+    * http://matplotlib.svn.sourceforge.net/viewvc/matplotlib\
+?view=revision&revision=6518
+    * http://matplotlib.sourceforge.net/api/api_changes.html#changes-for-0-98-x
+
+    .. note::
+        For details on all arguments see :func:`matplotlib.mlab.psd`.
+
+    .. note::
+        When using `window=welch_taper` (:func:`obspy.signal.psd.welch_taper`)
+        and `detrend=detrend_linear` (:func:`matplotlib.mlab.detrend_linear`)
+        the psd function delivers practically the same results as PITSA.
+        Only DC and the first 3-4 lowest non-DC frequencies deviate very
+        slightly. In contrast to PITSA, this routine also returns the psd value
+        at the Nyquist frequency and therefore is one frequency sample longer.
+    """
+    # check if matplotlib is available, no official dependency for obspy.signal
+    if MATPLOTLIB_VERSION is None:
+        raise ImportError(msg_matplotlib_ImportError)
+
+    # check matplotlib version
+    elif MATPLOTLIB_VERSION >= [0, 98, 4]:
+        new_matplotlib = True
+    else:
+        new_matplotlib = False
+    # build up kwargs that do not change with version 0.98.4
+    kwargs = {}
+    kwargs['NFFT'] = NFFT
+    kwargs['Fs'] = Fs
+    kwargs['detrend'] = detrend
+    kwargs['window'] = window
+    kwargs['noverlap'] = noverlap
+    # add additional kwargs to control behavior for matplotlib versions higher
+    # than 0.98.4. These settings make sure that the scaling is already done
+    # during the following psd call for newer matplotlib versions.
+    if new_matplotlib:
+        kwargs['pad_to'] = None
+        kwargs['sides'] = 'onesided'
+        kwargs['scale_by_freq'] = True
+    # do the actual call to mlab.psd
+    Pxx, freqs = mlab.psd(x, **kwargs)
+    # do scaling manually for old matplotlib versions
+    if not new_matplotlib:
+        Pxx = Pxx / Fs
+        Pxx[1:-1] = Pxx[1:-1] * 2.0
+    return Pxx, freqs
--- javelin/testJAVELIN.py
+++ javelin/testJAVELIN.py
@@ -1,31 +1,55 @@
-#Last-modified: 17 Jan 2012 12:34:33 AM
+#Last-modified: 17 Jan 2012 12:16:13 PM
 import numpy as np
 from predict import Predict
+import matplotlib.pyplot as plt
+
 
 """
 Test from scratch.
 """
 
-def genMockLC(lcfile):
+def genMockLC(j, tau, sigma, lcmean, emean):
     """ generate mock continuum light curves
     """
     covfunc = "drw"
-    tau, sigma = 50.0, 0.5
-    lcmean = 10.0
-    emean  = lcmean*0.05
     P = Predict(lcmean=lcmean, covfunc=covfunc, tau=tau, sigma=sigma)
-    j = np.concatenate([np.arange(0, 140, 1), np.arange(140+180, 2*140+180, 1)])
     ewant  = emean*np.ones_like(j)
     mwant  = P.generate(j, nlc=1, ewant=ewant, errcov=0.0)
-    np.savetxt(lcfile, np.vstack((j, mwant, ewant)).T)
     mve, var = P.mve_var(j)
     sig = np.sqrt(var)
-    return(mve, sig)
+    return(mwant, ewant, mve, sig)
 
+def showSnake(ax, j, mwant, ewant, mve, sig):
+    x = np.concatenate((j, j[::-1]))
+    y = np.concatenate((mve-sig, (mve+sig)[::-1]))
+    ax.fill(x,y,facecolor='.8',edgecolor='1.')
+    ax.plot(j, mve, 'k-.')
+    ax.errorbar(j, mwant, yerr=ewant, ecolor='k', ms=6, mfc='r', mec='k',
+            ls='None')
+    ax.set_xlim(j[0], j[-1])
 
 def main():
-    lcfile = "mock.dat"
-    mve, sig = genMockLC(lcfile)
+    if False :
+        lcfile = "mock_realistic.dat"
+        j = np.concatenate([np.arange(0, 140, 1), np.arange(140+180, 2*140+180, 1)])
+        tau, sigma, lcmean = 50.0, 0.5, 10.0
+        emean = lcmean*0.05
+        mwant, ewant, mve, sig = genMockLC(j, tau, sigma, lcmean, emean)
+        np.savetxt(lcfile, np.vstack((j, mwant, ewant)).T)
+    if True :
+        lcfile = "mock_dense.dat"
+        j = np.arange(0, 100, 0.01)
+        tau, sigma, lcmean = 50.0, 0.5, 10.0
+        emean = lcmean*0.05
+        mwant, ewant, mve, sig = genMockLC(j, tau, sigma, lcmean, emean)
+        np.savetxt(lcfile, np.vstack((j, mwant, ewant)).T)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    showSnake(ax, j, mwant, ewant, mve, sig)
+    plt.show()
+
+    fig.clf()
     pass
 
 if __name__ == "__main__":    

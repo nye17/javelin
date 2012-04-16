a3c28f6d3d0c47194f6dbb6e9aef4b1d9b3cad83
Ying <nye17@users.noreply.github.com>
2012-04-16 16:49:43 -0400

    writing the README file

--- examples/demo.py
+++ examples/demo.py
@@ -1,26 +1,30 @@
-#Last-modified: 12 Apr 2012 10:39:01 PM
+#Last-modified: 16 Apr 2012 04:38:20 PM
 import numpy as np
-from predict import PredictSignal, PredictRmap, generateLine, generateError
-from psd import psd
-from lcio import *
-from zylc import LightCurve, get_data
-from lcmodel import Cont_Model, Rmap_Model
 import matplotlib.pyplot as plt
+from javelin.predict import PredictSignal, PredictRmap, generateLine, generateError
+from javelin.lcio import *
+from javelin.zylc import LightCurve, get_data
+from javelin.lcmodel import Cont_Model, Rmap_Model
 
 """
 Test from scratch.
 """
 
+#************** DO NOT EDIT THIS PART************
+# names of the example light curves
 names  = ["Continuum", "Yelm", "Zing"]
+# smapling properties of the underlying signal
 jdense = np.linspace(0.0, 2000.0, 4000)
+# DRW parameters
 sigma, tau = (2.00, 100.0)
-tau_cut = 10.0
+# line parameters
 lagy, widy, scaley = (120.0,  3.0, 2.0)
 lagz, widz, scalez = (250.0,  9.0, 0.5)
 lags   = [0.0,   lagy,   lagz]
 wids   = [0.0,   widy,   widz]
 scales = [1.0, scaley, scalez]
 lcmeans= [10.0,  15.0,    5.0]
+#************************************************
 
 def file_exists(fname) :
     try :
@@ -95,29 +99,39 @@ def True2Mock(zydata, lcmeans=lcmeans, sparse=[2, 4, 4], errfac=[0.05, 0.05,
     zymock = LightCurve(zylclist_new, names=names)
     return(zymock)
 
-def getTrue(trufile, set_plot=False):
-    if file_exists(trufile) :
+def getTrue(trufile, set_plot=False, mode="test"):
+    if mode == "test" :
+        return(None)
+    elif mode == "show" :
         print("read true light curve signal from %s"%trufile)
         zydata = get_data(trufile, names=names)
-    else :
+    elif mode == "run" :
         print("generate true light curve signal")
         zydata = generateTrueLC(covfunc="drw")
         print("save true light curve signal to %s"%trufile)
+        trufile = ".".join([trufile, "myrun"])
         zydata.save(trufile)
     if set_plot :
         print("plot true light curve signal")
         zydata.plot()
     return(zydata)
 
-def getMock(zydata, confile, topfile, doufile, set_plot=False) :
+def getMock(zydata, confile, topfile, doufile, set_plot=False, mode="test") :
     # downsample the truth to get more realistic light curves
-    zydata_dou = True2Mock(zydata, lcmeans=lcmeans, sparse=[20, 20, 20], 
-        errfac=[0.05, 0.05, 0.05], set_seasongap=True)
-    zydata_dou.save_continuum(confile)
-    zydata_dou.save(doufile)
-    zylclist_top = zydata_dou.zylclist[:2]
-    zydata_top = LightCurve(zylclist_top, names=names[0:2])
-    zydata_top.save(topfile)
+    if mode == "test" :
+        return(None)
+    else :
+        zydata_dou = True2Mock(zydata, lcmeans=lcmeans, sparse=[20, 20, 20], 
+            errfac=[0.05, 0.05, 0.05], set_seasongap=True)
+        zylclist_top = zydata_dou.zylclist[:2]
+        zydata_top = LightCurve(zylclist_top, names=names[0:2])
+        if mode == "run" :
+            confile = ".".join([confile, "myrun"])
+            doufile = ".".join([doufile, "myrun"])
+            topfile = ".".join([topfile, "myrun"])
+            zydata_dou.save_continuum(confile)
+            zydata_dou.save(doufile)
+            zydata_top.save(topfile)
     if set_plot :
         print("plot mock light curves for continuum and yelm line")
         zydata_top.plot()
@@ -125,28 +139,46 @@ def getMock(zydata, confile, topfile, doufile, set_plot=False) :
         zydata_dou.plot()
 
 def fitCon(confile, confchain, names=None, threads=1, set_plot=False, nwalkers=100,
-        nburn=50, nchain=50, figext=None) :
+        nburn=50, nchain=50, figext=None, mode="test") :
+    if mode == "run" :
+        confile = ".".join([confile, "myrun"])
     zydata = get_data(confile, names=names)
     cont   = Cont_Model(zydata, "drw")
-    if file_exists(confchain) :
+    if mode == "test" :
+        print(cont([np.log(2.), np.log(100)]))
+        return(None)
+    elif mode == "show" :
         cont.load_chain(confchain)
-    else :
+    elif mode == "run" :
+        confchain = ".".join([confchain, "myrun"])
         cont.do_mcmc(nwalkers=nwalkers, nburn=nburn, nchain=nchain, fburn=None,
-                fchain=confchain, threads=threads)
+                fchain=confchain, threads=1)
     if set_plot :
         cont.show_hist(bins=100, figext=figext)
     return(cont.hpd)
 
-
 def fitLag(linfile, linfchain, conthpd, names=None, 
-        laglimit="baseline", lagrange=[100, 300], lagbinsize=1, 
+        lagrange=[50, 350], lagbinsize=1, 
         threads=1, set_plot=False, nwalkers=100, nburn=50, nchain=50,
-        figext=None) :
+        figext=None, mode="test") :
+    if mode == "run" :
+        linfile = ".".join([linfile, "myrun"])
     zydata = get_data(linfile, names=names)
     rmap   = Rmap_Model(zydata)
-    if file_exists(linfchain) :
+    if mode == "test" :
+        if zydata.nlc == 2 :
+            print(rmap([np.log(2.), np.log(100), lagy, widy, scaley ]))
+        elif zydata.nlc == 3 :
+            print(rmap([np.log(2.), np.log(100), lagy, widy, scaley, lagz, widz,
+                scalez]))
+        return(None)
+    elif mode == "show" :
         rmap.load_chain(linfchain, set_verbose=False)
-    else :
+    elif mode == "run" :
+        laglimit = [[0.0, 400.0],]*(zydata.nlc-1)
+        print(laglimit)
+#        laglimit = "baseline"
+        linfchain = ".".join([linfchain, "myrun"])
         rmap.do_mcmc(conthpd=conthpd, laglimit=laglimit, 
                 nwalkers=nwalkers, nburn=nburn, nchain=nchain,
                 fburn=None, fchain=linfchain, threads=threads)
@@ -158,29 +190,52 @@ def fitLag(linfile, linfchain, conthpd, names=None,
 
 
 if __name__ == "__main__":    
-    set_plot = True
-    threads  = 2
+    import sys
+    try:
+        import multiprocessing
+        threads = multiprocessing.cpu_count()
+    except (ImportError,NotImplementedError):
+        threads = 1
+        pass
+    if threads > 1 :
+        print("use multiprocessing on %d cpus"%threads)
+    else :
+        print("use single cpu")
+
+    mode = sys.argv[1]
+    if mode == "test" :
+        set_plot = False
+    elif mode == "show" :
+        set_plot = True
+    elif mode == "run" :
+        set_plot = True
+
+    # specified file names
+    trufile   = "dat/trulc.dat"
+    confile   = "dat/loopdeloop_con.dat"
+    topfile   = "dat/loopdeloop_con_y.dat"
+    doufile   = "dat/loopdeloop_con_y_z.dat"
+    confchain = "dat/chain0.dat"
+    topfchain = "dat/chain1.dat"
+    doufchain = "dat/chain2.dat"
+
     # generate truth drw signal
-    trufile = "dat/trulc.dat"
-#    zydata  = getTrue(trufile, set_plot=set_plot)
+    zydata  = getTrue(trufile, set_plot=set_plot, mode=mode)
 
     # generate mock light curves
-    confile = "dat/loopdeloop_con.dat"
-    topfile = "dat/loopdeloop_con_y.dat"
-    doufile = "dat/loopdeloop_con_y_z.dat"
-#    getMock(zydata, confile, topfile, doufile, set_plot=set_plot)
+    getMock(zydata, confile, topfile, doufile, set_plot=set_plot, mode=mode)
 
     # fit continuum
-    confchain = "dat/chain0.dat"
-    conthpd = fitCon(confile, confchain, threads=threads, set_plot=set_plot)
+    conthpd = fitCon(confile, confchain, threads=threads, set_plot=set_plot,
+            mode=mode)
 
     # fit tophat
-    topfchain = "dat/chain1.dat"
-    tophpd = fitLag(topfile, topfchain, conthpd, threads=threads, set_plot=set_plot)
+    tophpd = fitLag(topfile, topfchain, conthpd, threads=threads,
+            set_plot=set_plot, mode=mode)
 
     # fit douhat
-    doufchain = "dat/chain2.dat"
-    douhpd = fitLag(doufile, doufchain, conthpd, threads=threads, set_plot=set_plot)
+    douhpd = fitLag(doufile, doufchain, conthpd, threads=threads,
+            set_plot=set_plot, mode=mode)
 
 
 

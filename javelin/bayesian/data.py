8510965d53e445123b8900d67d275469e8cb4c22
Ying Zu <zuying@gmail.com>
2013-12-04 14:14:55 -0500

    removed the redundant dir 'bayesion'

--- javelin/bayesian/data.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import javelin.lcio as IO
-from javelin.zylc import LightCurve
-
-def get_data(lcfile):
-    lclist = IO.readlc_3c(lcfile)
-    zydata = LightCurve(lclist)
-    return(zydata)

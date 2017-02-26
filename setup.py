b5d1be08de608007e18a78165b52c0d9d28e8861
Ying <nye17@users.noreply.github.com>
2017-02-26 17:44:22 -0500

    minor prompt added to demo

--- setup.py
+++ setup.py
@@ -41,6 +41,10 @@ for arg in sys.argv :
 # If optimized lapack/ BLAS libraries are present, compile distributions that involve linear algebra against those.
 # Otherwise compile blas and lapack from netlib sources.
 lapack_info = get_info('lapack_opt',1)
+# lapack_info = get_info('',1)
+# lapack_info = False
+print lapack_info
+# quit()
 
 
 

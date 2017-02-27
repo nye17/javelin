8a985a810dbdb447fa4693a0b41ce0d6bfb13ff3
Ying <nye17@users.noreply.github.com>
2017-02-27 18:00:58 -0500

    add debugging threading.py

---
+++ examples/debug_threading.py
@@ -0,0 +1,17 @@
+from javelin.zylc import get_data
+from javelin.lcmodel import Cont_Model
+from javelin.lcmodel import Rmap_Model
+
+
+for i in xrange(50):
+	c = get_data(["dat/con.dat"])
+	cmod = Cont_Model(c)
+	cmod.do_mcmc(nwalkers=200, nburn=100, nchain=200, threads = 8)
+	a = cmod.get_hpd()
+	javdata4 = get_data(["dat/con.dat", "dat/yelm.dat", "dat/zing.dat"], names=["Continuum", "Yelm", "Zing"])
+	rmap2 = Rmap_Model(javdata4)
+	rmap2.do_mcmc(conthpd=a, nwalkers=200, nburn=100, nchain=200, threads = 8)
+
+	del rmap2
+	del c
+	print i

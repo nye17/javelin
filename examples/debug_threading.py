530ab501cb3dcfe3cdb6e69a7ddde7cce2bee6ba
Ying Zu <zuying@gmail.com>
2017-02-28 15:18:38 -0500

    remove con.dat to avoid problems under windows os

--- examples/debug_threading.py
+++ examples/debug_threading.py
@@ -4,11 +4,11 @@ from javelin.lcmodel import Rmap_Model
 
 
 for i in xrange(50):
-	c = get_data(["dat/con.dat"])
+	c = get_data(["dat/continuum.dat"])
 	cmod = Cont_Model(c)
 	cmod.do_mcmc(nwalkers=200, nburn=100, nchain=200, threads = 8)
 	a = cmod.get_hpd()
-	javdata4 = get_data(["dat/con.dat", "dat/yelm.dat", "dat/zing.dat"], names=["Continuum", "Yelm", "Zing"])
+	javdata4 = get_data(["dat/continuum.dat", "dat/yelm.dat", "dat/zing.dat"], names=["Continuum", "Yelm", "Zing"])
 	rmap2 = Rmap_Model(javdata4)
 	rmap2.do_mcmc(conthpd=a, nwalkers=200, nburn=100, nchain=200, threads = 8)
 

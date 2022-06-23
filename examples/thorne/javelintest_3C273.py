5e9869857a876f90d74b5d72d9d59d31affe0bfb
Ying Zu <zuying@gmail.com>
2022-06-23 22:43:22 +0800

    no bug detected so far

--- examples/thorne/javelintest_3C273.py
+++ examples/thorne/javelintest_3C273.py
@@ -7,16 +7,16 @@ import os.path
 c = get_data(["3C273_Bband.dat"])
 cmod = Cont_Model(c, "drw")
 cmod.do_mcmc()
-cmod.show_hist(figout='3C273_Bband_sig-tau_distributions', figext= '/Users/jamesthorne/javelin/3C273/3C273_Bband_sig-tau_distributions.png')
+cmod.show_hist(figout='3C273_Bband_sig-tau_distributions', figext= 'pdf')
 
 cy = get_data(["3C273_Bband.dat", "3C273_Gband.dat"], names=["B band", "G band"])
 cymod = Rmap_Model(cy)
 cymod.do_mcmc(conthpd=cmod.hpd, laglimit=[[1,10]], fchain='3C273_BGchain.dat')
 
-cymod.show_hist(figout='3C273_BGdistributions', figext= '/Users/jamesthorne/javelin/3C273/3C273_BGdistributions.png')
+cymod.show_hist(figout='3C273_BGdistributions', figext= 'pdf')
 
 par_best = cymod.hpd[1,:]
 javdata_best = cymod.do_pred(par_best)
 javdata_best.names=cy.names
-javdata_best.plot(set_pred=True,obs=cy,figout='3C273_BGgraphs', figext= '/Users/jamesthorne/javelin/3C273/3C273_BGgraphs.png')
+javdata_best.plot(set_pred=True,obs=cy,figout='3C273_BGgraphs', figext= 'pdf')
 
